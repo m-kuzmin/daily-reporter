2dbfbe25afdff492d4c53592ab1867918390f425
m-kuzmin <71077087+m-kuzmin@users.noreply.github.com>
2023-06-28 16:23:08 +0300

    feat: Added inline keyboard response

--- internal/clients/telegram/response/response.go
+++ internal/clients/telegram/response/response.go
@@ -1,9 +1,12 @@
 package response
 
 import (
+	"encoding/json"
+	"log"
 	"net/url"
 
 	"github.com/m-kuzmin/daily-reporter/internal/util/option"
+	"github.com/pkg/errors"
 )
 
 type BotAction interface {
@@ -27,6 +30,7 @@ type SendMessage struct {
 	Text           string
 	ParseMode      option.Option[string]
 	WebpagePreview bool
+	ReplyMarkup    ReplyMarkupper
 }
 
 type ChatID string
@@ -69,5 +73,53 @@ func (m SendMessage) URLEncode() (string, url.Values) {
 		params.Set("disable_web_page_preview", "true")
 	}
 
+	if m.ReplyMarkup != nil {
+		json, err := m.ReplyMarkup.ReplyMarkupJSON()
+		if err != nil {
+			log.Printf("While marshaling reply markup: %s", err)
+		} else {
+			params.Set("reply_markup", string(json))
+		}
+	}
+
 	return endpoint, params
+}
+
+func (m SendMessage) SetReplyMarkup(markup [][]InlineKeyboardButton) SendMessage {
+	m.ReplyMarkup = InlineKeyboardMarkup{Keyboard: markup}
+
+	return m
+}
+
+type ReplyMarkupper interface {
+	ReplyMarkupJSON() ([]byte, error)
+}
+
+type InlineKeyboardMarkup struct {
+	Keyboard [][]InlineKeyboardButton `json:"inline_keyboard"`
+}
+
+func (k InlineKeyboardMarkup) ReplyMarkupJSON() ([]byte, error) {
+	marshaled, err := json.Marshal(k)
+
+	return marshaled, errors.Wrap(err, "while marshaling InlineKeyboardMarkup to JSON")
+}
+
+/*
+Only one `Option` should be `Some` and the doc comment on the option explains what it does. The text is always present
+and is the button label
+*/
+type InlineKeyboardButton struct {
+	// Button label
+	Text string `json:"text"`
+
+	// Pressing this button makes the user type "@Bot (string)" in the current chat, or just the bot's username.
+	SwitchInlineQueryCurrentChat option.Option[string] `json:"switch_inline_query_current_chat"`
+}
+
+func InlineButtonSwitchQueryCurrentChat(text, query string) InlineKeyboardButton {
+	return InlineKeyboardButton{
+		Text:                         text,
+		SwitchInlineQueryCurrentChat: option.Some(query),
+	}
 }
